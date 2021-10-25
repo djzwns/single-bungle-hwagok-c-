3f7a0d3c8a0d6b535ddb862d07227d028658cc5d
hjjshg <hjjshg@game.hoseo.edu>
2021-10-25 16:49:29 +0900

    Update: first c# class

--- FirstHelloWorld/Form1.Designer.cs
+++ FirstHelloWorld/Form1.Designer.cs
@@ -31,15 +31,39 @@ namespace FirstHelloWorld
         {
             this.btnHello = new System.Windows.Forms.Button();
             this.btnChange = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.tbLeft = new System.Windows.Forms.TextBox();
+            this.tbRight = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.btnEqual = new System.Windows.Forms.Button();
+            this.btnTotal = new System.Windows.Forms.Button();
+            this.btnEven = new System.Windows.Forms.Button();
+            this.btnTry = new System.Windows.Forms.Button();
+            this.tbKor = new System.Windows.Forms.TextBox();
+            this.labelTotal = new System.Windows.Forms.Label();
+            this.labelTotalTxt = new System.Windows.Forms.Label();
+            this.labelClass = new System.Windows.Forms.Label();
+            this.tbEng = new System.Windows.Forms.TextBox();
+            this.tbMath = new System.Windows.Forms.TextBox();
+            this.labelAverage = new System.Windows.Forms.Label();
+            this.labelAverageTxt = new System.Windows.Forms.Label();
+            this.labelGrade = new System.Windows.Forms.Label();
+            this.labelGradeTxt = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnHello
             // 
             this.btnHello.BackColor = System.Drawing.Color.Salmon;
             this.btnHello.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnHello.Location = new System.Drawing.Point(104, 131);
+            this.btnHello.Location = new System.Drawing.Point(226, 56);
             this.btnHello.Name = "btnHello";
-            this.btnHello.Size = new System.Drawing.Size(210, 106);
+            this.btnHello.Size = new System.Drawing.Size(182, 57);
             this.btnHello.TabIndex = 0;
             this.btnHello.Text = "Button";
             this.btnHello.UseVisualStyleBackColor = false;
@@ -47,24 +71,274 @@ namespace FirstHelloWorld
             // 
             // btnChange
             // 
-            this.btnChange.Location = new System.Drawing.Point(104, 42);
+            this.btnChange.Location = new System.Drawing.Point(226, 14);
             this.btnChange.Name = "btnChange";
-            this.btnChange.Size = new System.Drawing.Size(210, 48);
+            this.btnChange.Size = new System.Drawing.Size(182, 36);
             this.btnChange.TabIndex = 1;
             this.btnChange.Text = "속성 변경";
             this.btnChange.UseVisualStyleBackColor = true;
             this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
             // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(24, 119);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 31);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Location = new System.Drawing.Point(105, 119);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(75, 31);
+            this.btnSub.TabIndex = 3;
+            this.btnSub.Text = "Sub";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnSub_Click);
+            // 
+            // btnMul
+            // 
+            this.btnMul.Location = new System.Drawing.Point(24, 156);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(75, 31);
+            this.btnMul.TabIndex = 4;
+            this.btnMul.Text = "Mul";
+            this.btnMul.UseVisualStyleBackColor = true;
+            this.btnMul.Click += new System.EventHandler(this.btnMul_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(105, 156);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(75, 31);
+            this.btnDiv.TabIndex = 5;
+            this.btnDiv.Text = "Div";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Location = new System.Drawing.Point(21, 35);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(57, 15);
+            this.labelTitle.TabIndex = 6;
+            this.labelTitle.Text = "결과값:";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(79, 35);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(15, 15);
+            this.labelResult.TabIndex = 7;
+            this.labelResult.Text = "0";
+            // 
+            // tbLeft
+            // 
+            this.tbLeft.Location = new System.Drawing.Point(24, 88);
+            this.tbLeft.Name = "tbLeft";
+            this.tbLeft.Size = new System.Drawing.Size(75, 25);
+            this.tbLeft.TabIndex = 8;
+            this.tbLeft.Text = "0";
+            this.tbLeft.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // tbRight
+            // 
+            this.tbRight.Location = new System.Drawing.Point(105, 88);
+            this.tbRight.Name = "tbRight";
+            this.tbRight.Size = new System.Drawing.Size(75, 25);
+            this.tbRight.TabIndex = 8;
+            this.tbRight.Text = "0";
+            this.tbRight.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(24, 67);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(92, 15);
+            this.label.TabIndex = 9;
+            this.label.Text = "값 두개 입력";
+            // 
+            // btnEqual
+            // 
+            this.btnEqual.Location = new System.Drawing.Point(24, 193);
+            this.btnEqual.Name = "btnEqual";
+            this.btnEqual.Size = new System.Drawing.Size(75, 31);
+            this.btnEqual.TabIndex = 10;
+            this.btnEqual.Text = "Equal";
+            this.btnEqual.UseVisualStyleBackColor = true;
+            this.btnEqual.Click += new System.EventHandler(this.btnEqual_Click);
+            // 
+            // btnTotal
+            // 
+            this.btnTotal.Location = new System.Drawing.Point(105, 193);
+            this.btnTotal.Name = "btnTotal";
+            this.btnTotal.Size = new System.Drawing.Size(75, 31);
+            this.btnTotal.TabIndex = 10;
+            this.btnTotal.Text = "Total";
+            this.btnTotal.UseVisualStyleBackColor = true;
+            this.btnTotal.Click += new System.EventHandler(this.btnTotal_Click);
+            // 
+            // btnEven
+            // 
+            this.btnEven.Location = new System.Drawing.Point(226, 119);
+            this.btnEven.Name = "btnEven";
+            this.btnEven.Size = new System.Drawing.Size(75, 31);
+            this.btnEven.TabIndex = 10;
+            this.btnEven.Text = "Even";
+            this.btnEven.UseVisualStyleBackColor = true;
+            this.btnEven.Click += new System.EventHandler(this.btnEven_Click);
+            // 
+            // btnTry
+            // 
+            this.btnTry.Location = new System.Drawing.Point(226, 156);
+            this.btnTry.Name = "btnTry";
+            this.btnTry.Size = new System.Drawing.Size(75, 31);
+            this.btnTry.TabIndex = 10;
+            this.btnTry.Text = "Try";
+            this.btnTry.UseVisualStyleBackColor = true;
+            this.btnTry.Click += new System.EventHandler(this.btnTry_Click);
+            // 
+            // tbKor
+            // 
+            this.tbKor.Location = new System.Drawing.Point(24, 293);
+            this.tbKor.Name = "tbKor";
+            this.tbKor.Size = new System.Drawing.Size(38, 25);
+            this.tbKor.TabIndex = 8;
+            this.tbKor.Text = "0";
+            this.tbKor.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // labelTotal
+            // 
+            this.labelTotal.AutoSize = true;
+            this.labelTotal.Location = new System.Drawing.Point(168, 296);
+            this.labelTotal.Name = "labelTotal";
+            this.labelTotal.Size = new System.Drawing.Size(42, 15);
+            this.labelTotal.TabIndex = 6;
+            this.labelTotal.Text = "총점:";
+            // 
+            // labelTotalTxt
+            // 
+            this.labelTotalTxt.AutoSize = true;
+            this.labelTotalTxt.Location = new System.Drawing.Point(216, 296);
+            this.labelTotalTxt.Name = "labelTotalTxt";
+            this.labelTotalTxt.Size = new System.Drawing.Size(15, 15);
+            this.labelTotalTxt.TabIndex = 7;
+            this.labelTotalTxt.Text = "0";
+            // 
+            // labelClass
+            // 
+            this.labelClass.AutoSize = true;
+            this.labelClass.Location = new System.Drawing.Point(24, 275);
+            this.labelClass.Name = "labelClass";
+            this.labelClass.Size = new System.Drawing.Size(129, 15);
+            this.labelClass.TabIndex = 9;
+            this.labelClass.Text = "국어 / 영어 / 수학";
+            // 
+            // tbEng
+            // 
+            this.tbEng.Location = new System.Drawing.Point(68, 293);
+            this.tbEng.Name = "tbEng";
+            this.tbEng.Size = new System.Drawing.Size(38, 25);
+            this.tbEng.TabIndex = 8;
+            this.tbEng.Text = "0";
+            this.tbEng.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // tbMath
+            // 
+            this.tbMath.Location = new System.Drawing.Point(112, 293);
+            this.tbMath.Name = "tbMath";
+            this.tbMath.Size = new System.Drawing.Size(38, 25);
+            this.tbMath.TabIndex = 8;
+            this.tbMath.Text = "0";
+            this.tbMath.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // labelAverage
+            // 
+            this.labelAverage.AutoSize = true;
+            this.labelAverage.Location = new System.Drawing.Point(168, 318);
+            this.labelAverage.Name = "labelAverage";
+            this.labelAverage.Size = new System.Drawing.Size(42, 15);
+            this.labelAverage.TabIndex = 6;
+            this.labelAverage.Text = "평균:";
+            // 
+            // labelAverageTxt
+            // 
+            this.labelAverageTxt.AutoSize = true;
+            this.labelAverageTxt.Location = new System.Drawing.Point(216, 318);
+            this.labelAverageTxt.Name = "labelAverageTxt";
+            this.labelAverageTxt.Size = new System.Drawing.Size(15, 15);
+            this.labelAverageTxt.TabIndex = 7;
+            this.labelAverageTxt.Text = "0";
+            // 
+            // labelGrade
+            // 
+            this.labelGrade.AutoSize = true;
+            this.labelGrade.Location = new System.Drawing.Point(168, 340);
+            this.labelGrade.Name = "labelGrade";
+            this.labelGrade.Size = new System.Drawing.Size(42, 15);
+            this.labelGrade.TabIndex = 6;
+            this.labelGrade.Text = "평가:";
+            // 
+            // labelGradeTxt
+            // 
+            this.labelGradeTxt.AutoSize = true;
+            this.labelGradeTxt.Location = new System.Drawing.Point(216, 340);
+            this.labelGradeTxt.Name = "labelGradeTxt";
+            this.labelGradeTxt.Size = new System.Drawing.Size(15, 15);
+            this.labelGradeTxt.TabIndex = 7;
+            this.labelGradeTxt.Text = "0";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(24, 324);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(126, 31);
+            this.btnCheck.TabIndex = 10;
+            this.btnCheck.Text = "Score Button";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnScore_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(411, 249);
+            this.ClientSize = new System.Drawing.Size(434, 400);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.btnTry);
+            this.Controls.Add(this.btnEven);
+            this.Controls.Add(this.btnTotal);
+            this.Controls.Add(this.btnEqual);
+            this.Controls.Add(this.labelClass);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.tbRight);
+            this.Controls.Add(this.tbMath);
+            this.Controls.Add(this.tbEng);
+            this.Controls.Add(this.tbKor);
+            this.Controls.Add(this.tbLeft);
+            this.Controls.Add(this.labelGradeTxt);
+            this.Controls.Add(this.labelAverageTxt);
+            this.Controls.Add(this.labelTotalTxt);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.labelGrade);
+            this.Controls.Add(this.labelAverage);
+            this.Controls.Add(this.labelTotal);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMul);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.btnChange);
             this.Controls.Add(this.btnHello);
             this.Name = "Form1";
-            this.Text = "Hello Form";
+            this.Text = "사칙연산";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -72,6 +346,30 @@ namespace FirstHelloWorld
 
         private System.Windows.Forms.Button btnHello;
         private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.TextBox tbLeft;
+        private System.Windows.Forms.TextBox tbRight;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button btnEqual;
+        private System.Windows.Forms.Button btnTotal;
+        private System.Windows.Forms.Button btnEven;
+        private System.Windows.Forms.Button btnTry;
+        private System.Windows.Forms.TextBox tbKor;
+        private System.Windows.Forms.Label labelTotal;
+        private System.Windows.Forms.Label labelTotalTxt;
+        private System.Windows.Forms.Label labelClass;
+        private System.Windows.Forms.TextBox tbEng;
+        private System.Windows.Forms.TextBox tbMath;
+        private System.Windows.Forms.Label labelAverage;
+        private System.Windows.Forms.Label labelAverageTxt;
+        private System.Windows.Forms.Label labelGrade;
+        private System.Windows.Forms.Label labelGradeTxt;
+        private System.Windows.Forms.Button btnCheck;
     }
 }
 
--- FirstHelloWorld/Form1.cs
+++ FirstHelloWorld/Form1.cs
@@ -19,12 +19,154 @@ namespace FirstHelloWorld
 
         private void btnHello_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello World!!", "Hello Message Box", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            string message = btnHello.Text == "Hello" ? "Hello World!!" : "Button Clicked!";
+            MessageBox.Show(message, "Hello Message Box", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
         }
 
         private void btnChange_Click(object sender, EventArgs e)
         {
+            //    int[] arr = { 1, 2, 3, 4 };
+            //    int[] initArr = { 6, 5, 4, 3, 2, 1 };
+            //    Console.WriteLine(arr);
+            //    Console.WriteLine("array 0 -> " + Convert.ToString(arr[0]));
+            //    arr = new int[100];
+            //    arr[0] = 0;
+            //    arr[1] = 1;
+            //    //Console.WriteLine("array 0 -> " + Convert.ToString(arr[0]));
+            int a = 5;
+            float b = 0;
+            b = (float)a;
+            Console.WriteLine(String.Format("b: {0}", b));
             btnHello.Text = btnHello.Text == "Hello" ? "Button" : "Hello";
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            float left = Convert.ToSingle(tbLeft.Text);
+            float right = Convert.ToSingle(tbRight.Text);
+
+            labelResult.Text = Convert.ToString(left + right);
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            float left = Convert.ToSingle(tbLeft.Text);
+            float right = Convert.ToSingle(tbRight.Text);
+
+            labelResult.Text = Convert.ToString(left - right);
+        }
+
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            float left = Convert.ToSingle(tbLeft.Text);
+            float right = Convert.ToSingle(tbRight.Text);
+
+            labelResult.Text = Convert.ToString(left * right);
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            float left = Convert.ToSingle(tbLeft.Text);
+            float right = Convert.ToSingle(tbRight.Text);
+            if (right == 0)
+            {
+                labelResult.Text = "오른쪽 값이 0이 되면 안됨니다";
+                return;
+            }
+
+            labelResult.Text = Convert.ToString(left / right);
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            float left = Convert.ToSingle(tbLeft.Text);
+            float right = Convert.ToSingle(tbRight.Text);
+
+            labelResult.Text = Convert.ToString(left == right);
+        }
+
+        private void btnTotal_Click(object sender, EventArgs e)
+        {
+            int left = Convert.ToInt32(tbLeft.Text);
+            int right = Convert.ToInt32(tbRight.Text);
+            if (left > right)
+            {
+                int t = left;
+                left = right;
+                right = t;
+            }
+
+            int sum = 0;
+            for (int i = left; i <= right; ++i)
+            {
+                sum += i;
+            }
+
+            labelResult.Text = Convert.ToString(sum);
+        }
+
+        private void btnEven_Click(object sender, EventArgs e)
+        {
+            int left = Convert.ToInt32(tbLeft.Text);
+            int right = Convert.ToInt32(tbRight.Text);
+            if (left > right)
+            {
+                int t = left;
+                left = right;
+                right = t;
+            }
+
+
+            if ((left & 1) == 1)
+                left += 1;
+
+                int sum = 0;
+            for (int i = left; i <= right; i += 2)
+            {
+                sum += i;
+            }
+
+            labelResult.Text = Convert.ToString(sum);
+        }
+
+        private void btnTry_Click(object sender, EventArgs e)
+        {
+            float left = Convert.ToSingle(tbLeft.Text);
+            float right = Convert.ToSingle(tbRight.Text);
+            float result = 0;
+
+            try
+            {
+                result = left / right;
+
+            } catch (DivideByZeroException error)
+            {
+                Console.WriteLine(error.Message);
+                labelResult.Text = error.Message;
+            } finally
+            {
+                labelResult.Text = Convert.ToString(result);
+            }
+        }
+
+        private void btnScore_Click(object sender, EventArgs e)
+        {
+            float kor = Convert.ToSingle(tbKor.Text);
+            float eng = Convert.ToSingle(tbEng.Text);
+            float math = Convert.ToSingle(tbMath.Text);
+
+            float total = kor + eng + math;
+            float avg = total / 3;
+            string grade = "";
+            if (avg >= 90) grade = "A";
+            else if (avg >= 80) grade = "B";
+            else if (avg >= 70) grade = "C";
+            else if (avg >= 60) grade = "D";
+            else grade = "F";
+
+            labelTotalTxt.Text = Convert.ToString(total);
+            labelAverageTxt.Text = Convert.ToString(avg);
+            labelGradeTxt.Text = grade;
         }
     }
 }
