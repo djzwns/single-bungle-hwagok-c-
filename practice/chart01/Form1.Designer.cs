98b0313985ab51288d8dec615eb61329008da6cd
hjjshg <hjjshg@game.hoseo.edu>
2021-11-02 16:19:08 +0900

    Add: chart practice

---
+++ practice/chart01/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace chart01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint4 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(1D, "5,0,0,0");
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint5 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(2D, "3,0,0,0");
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint6 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(3D, "7,0,0,0");
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tbxAxis = new System.Windows.Forms.TextBox();
+            this.tbyAxis = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.labelX = new System.Windows.Forms.Label();
+            this.labelY = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.IsValueShownAsLabel = true;
+            series2.IsVisibleInLegend = false;
+            series2.IsXValueIndexed = true;
+            series2.Name = "A";
+            series2.Points.Add(dataPoint4);
+            series2.Points.Add(dataPoint5);
+            series2.Points.Add(dataPoint6);
+            series2.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            series2.YValuesPerPoint = 4;
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(433, 316);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // tbxAxis
+            // 
+            this.tbxAxis.Location = new System.Drawing.Point(471, 80);
+            this.tbxAxis.Name = "tbxAxis";
+            this.tbxAxis.Size = new System.Drawing.Size(100, 25);
+            this.tbxAxis.TabIndex = 1;
+            // 
+            // tbyAxis
+            // 
+            this.tbyAxis.Location = new System.Drawing.Point(471, 111);
+            this.tbyAxis.Name = "tbyAxis";
+            this.tbyAxis.Size = new System.Drawing.Size(100, 25);
+            this.tbyAxis.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(471, 239);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Location = new System.Drawing.Point(450, 83);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(16, 15);
+            this.labelX.TabIndex = 4;
+            this.labelX.Text = "x";
+            // 
+            // labelY
+            // 
+            this.labelY.AutoSize = true;
+            this.labelY.Location = new System.Drawing.Point(451, 114);
+            this.labelY.Name = "labelY";
+            this.labelY.Size = new System.Drawing.Size(15, 15);
+            this.labelY.TabIndex = 5;
+            this.labelY.Text = "y";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 340);
+            this.Controls.Add(this.labelY);
+            this.Controls.Add(this.labelX);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tbyAxis);
+            this.Controls.Add(this.tbxAxis);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox tbxAxis;
+        private System.Windows.Forms.TextBox tbyAxis;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.Label labelY;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ practice/chart01/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chart01
+{
+    public partial class Form1 : Form
+    {
+        private Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            timer1.Interval = 100;
+            timer1.Tick += new EventHandler(ChartUpdate);
+            timer1.Start();
+        }
+
+        private void ChartUpdate(object sender, EventArgs e)
+        {
+            if (chart1.Series[0].Points.Count >= 50)
+            {
+                chart1.Series[0].Points.RemoveAt(0);
+            }
+            chart1.Series[0].Points.AddXY(DateTime.Now.ToString("mm:ss"), rand.Next(100));
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (chart1.Series[0].Points.Count >= 10)
+            {
+                chart1.Series[0].Points.RemoveAt(0);
+            }
+            chart1.Series[0].Points.AddXY(DateTime.Now.ToString("mm:ss"), rand.Next(100));
+            //Console.WriteLine($"addxy result: {val}");
+        }
+    }
+}
